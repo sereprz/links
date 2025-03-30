6b91a4bc426eb91b560cc6122f0128041c739c6b
Serena <serena@Serenas-MacBook-Air.local>
2025-03-29 22:48:57 -0400

    Added basic info

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   redirects: {
     "/blog": "/",
   },
-  site: "https://example.treelink.com",
+  site: "https://serenaperuzzo.com",
   integrations: [tailwind(), sitemap(), icon()],
 });
